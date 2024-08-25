da4460d1572120edba16932f93e25f195f6e9524
Akshaya Prabha A V <95605401+AtchayaPraba@users.noreply.github.com>
2024-08-25 10:56:51 +0530

    Added test.py
    Added test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import timm
+import torchgeo
+import lightning 
+
+if __name__ == '__main__':
+    print ("Hello World")
+    print (timm.__version__)
+    print (torchgeo.__version__)
+    print (lightning.__version__)
